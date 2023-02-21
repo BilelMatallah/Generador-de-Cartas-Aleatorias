2387dc99374160063a69316fb17cb3afeaed8366
BilelMatallah <bilelmatallah@hotmail.com>
2023-02-21 20:35:15 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.addEventListener("load", function() {
+  document.querySelector(".card").classList.add(simboloRandom());
+  document.querySelector(".card").innerHTML = numeroRandom();
+});
+
+let numeroRandom = () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumeros = Math.floor(Math.random() * numeros.length);
+  return numeros[indexNumeros];
+};
+
+let simboloRandom = () => {
+  let simbolos = ["spade", "club", "heart", "diam"];
+  let indexsimbolos = Math.floor(Math.random() * simbolos.length);
+  return simbolos[indexsimbolos];
 };
